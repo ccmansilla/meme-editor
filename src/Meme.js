e8a7129a7cd100712213e8240dd83e192bb5498b
ccmansilla <carloscmansilla@gmail.com>
2022-11-09 07:34:19 -0300

    recorto tamaño canva ajustado a la imagen

--- src/Meme.js
+++ src/Meme.js
@@ -4,9 +4,10 @@ import html2canvas from 'html2canvas';
 
 export const Meme = () => {
     let centroH = window.screen.width / 2;
+    let canvaX = centroH - 200;
 
     const [texto, setTexto] = useState();
-    const [colorTexto, setColor] = useState('#000');
+    const [colorTexto, setColor] = useState('#d7f700');
     const [sizeTexto, setSize] = useState(40);
     const [posicionHTexto, setPosicionH] = useState(centroH);
     const [posicionVTexto, setPosicionV] = useState(150);
@@ -24,7 +25,7 @@ export const Meme = () => {
     };
 
     const generar = () => {
-        html2canvas(document.querySelector('#meme'), { allowTaint: true, useCORS: true })
+        html2canvas(document.querySelector('#meme'), { allowTaint: true, useCORS: true, width: 400, x: canvaX})
             .then(function (canvas) {
                 let img = canvas.toDataURL("memes/jpg");
                 let link = document.createElement("a");
@@ -49,7 +50,7 @@ export const Meme = () => {
             <h1 className='py-3 titulo'>Meme Editor</h1>
             <div className='d-flex justify-content-center flex-wrap menu'>
                 <div className='px-2'>
-                    <h4>Imagen</h4>
+                    <h5>Imagen</h5>
                     <select name="img" defaultValue='0' onChange={selectImg}>
                         {imagenes.map((op, index, arr) =>
                             <option value={index}>{op.name}</option>
@@ -57,15 +58,15 @@ export const Meme = () => {
                     </select>
                 </div>
                 <div className='px-2'>
-                    <h4>Frase</h4>
+                    <h5>Frase</h5>
                     <input type="text" name="texto" onChange={setFrase} placeholder='texto aqui..' />
                 </div>
                 <div className='px-2'>
-                    <h4>Color</h4>
-                    <input type="color" onInput={(e) => setColor(e.target.value)} />
+                    <h5>Color</h5>
+                    <input type="color" value={colorTexto} onInput={(e) => setColor(e.target.value)} />
                 </div>
                 <div className='px-2'>
-                    <h4>Tamaño</h4>
+                    <h5>Tamaño</h5>
                     <select name='size' defaultValue='40' onChange={(e) => setSize(e.target.value)}>
                         <option>30</option>
                         <option>40</option>
@@ -80,20 +81,20 @@ export const Meme = () => {
 
             <div className='d-flex justify-content-center flex-wrap menu'>
                 <div className='px-2'>
-                    <h4>Posicion</h4>
+                    <h5>Posicion</h5>
                     <button className='btn btn-primary mx-1' onClick={() => setPosicionV(posicionVTexto + 10)} > ↓ </button>
                     <button className='btn btn-primary mx-1' onClick={() => setPosicionV(posicionVTexto - 10)} > ↑ </button>
                     <button className='btn btn-primary mx-1' onClick={() => setPosicionH(posicionHTexto - 10)} > ← </button>
                     <button className='btn btn-primary mx-1' onClick={() => setPosicionH(posicionHTexto + 10)} > → </button>
                 </div>
                 <div className='px-2'>
-                    <h4>Girar</h4>
+                    <h5>Girar</h5>
                     <button className='btn btn-primary mx-1' onClick={() => setRotate(rotate - 15)} > ←  </button>
                     <button className='btn btn-primary mx-1' onClick={() => setRotate(rotate + 15)} > → </button>
                 </div>
                 <div className='px-2'>
-                    <h4>Meme</h4>
-                    <button onClick={generar} type="button" className='btn btn-primary'>Descargar Meme</button>
+                    <h5>Meme</h5>
+                    <button onClick={generar} type="button" className='btn btn-primary'>Descargar</button>
                 </div>
             </div>
         </div>
