58a215b46a71d6a882b3fa1547f2b00af5a80254
ccmansilla <carloscmansilla@gmail.com>
2022-11-19 08:03:21 -0300

    mejoro la vista y agrego multiples frases

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,12 +3,26 @@ import Frase from './Frase';
 import React, { useState, useEffect } from 'react';
 import html2canvas from 'html2canvas';
 
+class Phrase{
+    constructor(text, x, y, angle, font, size, color) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.font = font;
+        this.size = size;
+        this.color = color;
+    }
+}
+
 export const Meme = () => {
 
-    const [texto, setTexto] = useState();
+    const [frases, setFrases] = useState([]);
+
+    const [texto, setTexto] = useState('');
     const [font, setFont] = useState('arial');
-    const [color, setColor] = useState('#000');
-    const [size, setSize] = useState('35px');
+    const [color, setColor] = useState('#ffffff');
+    const [size, setSize] = useState('30px');
     const [posX, setPosX] = useState(10);
     const [posY, setPosY] = useState(10);
     const [angle, setAngle] = useState(0);
@@ -23,6 +37,13 @@ export const Meme = () => {
     const selectImg = (e) => {
         setImagen(imagenes[e.target.value]);
     };
+
+    const addFrase = () => {
+        let nueva = new Phrase(texto, posX, posY, angle, font, size, color);
+        setFrases([...frases, nueva]);
+        setTexto('');
+        setPosY(posY + 80);
+    }
 
     const generar = () => {
         html2canvas(document.querySelector('#meme'), { allowTaint: true, useCORS: true, width: 400})
@@ -44,7 +65,6 @@ export const Meme = () => {
             })
     }, []);
 
-
     return (
         <div className='text-center'>
             <h1 className='py-3 titulo'>Meme Editor</h1>
@@ -59,7 +79,8 @@ export const Meme = () => {
                 </div>
                 <div className='px-2'>
                     <h5>Frase</h5>
-                    <input type="text" name="texto" onChange={setFrase} placeholder='texto aqui..' />
+                    <input type="text" name="texto" value={texto} onChange={setFrase} placeholder='texto aqui..' />
+                    <button className='btn btn-success mx-1' onClick={addFrase} > + </button>
                 </div>
                 <div className='px-2'>
                     <h5>Fuente</h5>
@@ -76,18 +97,21 @@ export const Meme = () => {
                 </div>
                 <div className='px-2'>
                     <h5>Color</h5>
-                    <input type="color" value={color} onInput={(e) => setColor(e.target.value)} />
+                    <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
                 </div>
                 <div className='px-2'>
                     <h5>Tamaño</h5>
-                    <select name='size' defaultValue='35px' onChange={(e) => setSize(e.target.value)}>
-                        <option value='25px'>Pequeño</option>
-                        <option value='35px'>Mediano</option>
-                        <option value='45px'>Grande</option>
+                    <select name='size' defaultValue='30px' onChange={(e) => setSize(e.target.value)}>
+                        <option value='20px'>Pequeño</option>
+                        <option value='30px'>Mediano</option>
+                        <option value='40px'>Grande</option>
                     </select>
                 </div>
             </div>
             <figure id='meme'>
+                {frases.map((f) =>
+                    <Frase text={f.text} font={f.font} size={f.size} x={f.x} y={f.y} color={f.color} angle={f.angle} />
+                )}
                 <Frase text={texto} font={font} size={size} x={posX} y={posY} color={color} angle={angle} />
                 <img src={imagen.url} alt="" className='img' />
             </figure>
@@ -95,19 +119,19 @@ export const Meme = () => {
             <div className='d-flex justify-content-center flex-wrap menu mt-2'>
                 <div className='px-2'>
                     <h5>Posicion</h5>
-                    <button className='btn btn-primary mx-1' onClick={() => setPosY(posY + 10)} > ↓ </button>
-                    <button className='btn btn-primary mx-1' onClick={() => setPosY(posY - 10)} > ↑ </button>
-                    <button className='btn btn-primary mx-1' onClick={() => setPosX(posX - 10)} > ← </button>
-                    <button className='btn btn-primary mx-1' onClick={() => setPosX(posX + 10)} > → </button>
+                    <button className='btn btn-success mx-1' onClick={() => setPosY(posY + 10)} > ↓ </button>
+                    <button className='btn btn-success mx-1' onClick={() => setPosY(posY - 10)} > ↑ </button>
+                    <button className='btn btn-success mx-1' onClick={() => setPosX(posX - 10)} > ← </button>
+                    <button className='btn btn-success mx-1' onClick={() => setPosX(posX + 10)} > → </button>
                 </div>
                 <div className='px-2'>
                     <h5>Girar</h5>
-                    <button className='btn btn-primary mx-1' onClick={() => setAngle(angle - 15)} > ←  </button>
-                    <button className='btn btn-primary mx-1' onClick={() => setAngle(angle + 15)} > → </button>
+                    <button className='btn btn-success mx-1' onClick={() => setAngle(angle - 15)} > ←  </button>
+                    <button className='btn btn-success mx-1' onClick={() => setAngle(angle + 15)} > → </button>
                 </div>
                 <div className='px-2'>
                     <h5>Meme</h5>
-                    <button onClick={generar} type="button" className='btn btn-primary'>Descargar</button>
+                    <button onClick={generar} type="button" className='btn btn-success'>Descargar</button>
                 </div>
             </div>
         </div>
