d828b1fe549439b33eb23d3b0d939b9f24b39ee8
ccmansilla <carloscmansilla@gmail.com>
2022-11-19 09:37:54 -0300

    agrego mis creditos

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Meme />
+      <div className='bg-success text-white p-5'>
+        <p className='text-center'>by <a href='https://www.linkedin.com/in/carloscmansilla/' style={{color: 'white'}} target='_blank'>Carlos Mansilla</a></p>
+      </div>
     </div>
   );
 }
